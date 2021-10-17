cbdf3dd641c5cdee4f8d10620d2bd7179fd6f099
Noah Le Roy <noah.le.roy@hva.nl>
2021-10-17 05:09:21 +0200

    removal

--- main.py
+++ main.py
@@ -6,21 +6,6 @@ st.set_page_config(
         initial_sidebar_state="expanded"
     )
 
-CURRENT_THEME = "blue"
-IS_DARK_THEME = True
-EXPANDER_TEXT = """
-    This is a custom theme. You can enable it by copying the following code
-    to `.streamlit/config.toml`:
-    ```python
-    [theme]
-    primaryColor = "#E694FF"
-    backgroundColor = "#00172B"
-    secondaryBackgroundColor = "#0083B8"
-    textColor = "#C6CDD4"
-    font = "sans-serif"
-    ```
-    """
-
 # Mainsss
 if __name__ == "__main__":
     import components.base as base
