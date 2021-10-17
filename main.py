5b1365169acb91b721b9964c71036c474687ba93
Noah Le Roy <noah.le.roy@hva.nl>
2021-10-17 04:07:32 +0200

    removal

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import streamlit as st
 st.set_page_config(
         page_title='VA 4',
         layout='wide',
-        initial_sidebar_state="expanded"
+        initial_sidebar_state="expanded",
+        theme= "Light"
     )
 
 # Mainsss
