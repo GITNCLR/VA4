dc5406850150915e2537b9cff4553c2c32bc79db
Noah Le Roy <noah.le.roy@hva.nl>
2021-10-17 04:57:08 +0200

    removal

--- utils/helpers.py
+++ utils/helpers.py
@@ -9,17 +9,17 @@ def fetchRDWData(isBrandstof=False):
     url = constants.RDW_KENTEKEN_URL
     if (isBrandstof):
         url = constants.RDW_BRANDSTOF_API_URL
-    response = rq.get(url, 
+    response = rq.get(url,
     headers={"X-App-Token": st.secrets['RDW_API_KEY']})
     df = pd.read_json(response.json())
 
 @st.cache
 def fetchOCMData(token, isFilters=False):
-    response = rq.get(constants.OCM_API_URL, 
+    response = rq.get(constants.OCM_API_URL,
     headers={"X-API-Key": token}, params={"maxresults": 500}).json()
     df = pd.json_normalize(response)
     fitlers = pd.json_normalize(response, record_path=["Connections"])
-    listOfCols = ['IsRecentlyVerified', 'UsageType.Title', 'AddressInfo.Latitude', 'AddressInfo.Longitude', 'AddressInfo.StateOrProvince', 
+    listOfCols = ['IsRecentlyVerified', 'UsageType.Title', 'AddressInfo.Latitude', 'AddressInfo.Longitude', 'AddressInfo.StateOrProvince',
     'Connections', 'NumberOfPoints', 'StatusTypeID']
     df['Connections'] = df['Connections'].apply(lambda x: [y['ConnectionType']['Title'] for y in x])
     df.drop(columns=df.columns[~df.columns.isin(listOfCols)].values, axis=1, inplace=True)
